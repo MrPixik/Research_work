38f3163f6a8fb728bb45a247599190a3ba1dee6d
pixik <chernyshev.sergey2003@gmail.com>
2024-12-11 13:46:18 +0300

    "Added 1. Added plotting topomap by intervals 2. Added saving topomap by intervals

--- cmd/eeg_processing/main.py
+++ cmd/eeg_processing/main.py
@@ -1,32 +1,9 @@
-import mne
-import matplotlib.pyplot as plt
-import matplotlib
-import numpy as np
+from app.encefalogram.EEGPreprocessor import *
+from app.encefalogram.DataAnalyzer import *
 
-matplotlib.use('TkAgg')
+if __name__ == '__main__':
+    # DataAnalyzer.show_topomap()
+    bands = [DataAnalyzer.alpha]
+    # DataAnalyzer.plot_topomap_by_band(bands)
+    DataAnalyzer.save_topomap_by_time(bands, save_images=True, output_dir=r'../../static/media/topomap_images')
 
-data_path = r'../../static/eeg_data/panina1.set'
-
-# Importing data and cropping for
-raw = mne.io.read_raw_eeglab(data_path, preload=True)
-# raw.crop(tmax=60).load_data()
-# fig1 = raw.plot(duration=30, remove_dc=False)
-# plt.show()
-
-# raw_filtered_low_freq_drifts = raw.copy().filter(l_freq=1.,h_freq=None)
-# fig2 = raw_filtered_low_freq_drifts.plot(duration=30,remove_dc=False)
-# fig2.suptitle("Filtered low frequency")
-
-
-# raw.plot()
-# plt.show()
-
-spectrum = raw.compute_psd()
-# spectrum.plot(average=False, picks="data", exclude="bads", amplitude=False)
-# plt.show()
-
-
-spectrum.plot_topomap()
-plt.show()
-# spectrum.plot_topo()
-# plt.show()
